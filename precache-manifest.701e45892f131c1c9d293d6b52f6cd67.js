bbda7f2db9bf9b8a04c3067f53bd4eaf56eaac40
uri121 <robinov121@gmail.com>
2020-08-08 21:55:37 +0300

    Updates

---
+++ precache-manifest.701e45892f131c1c9d293d6b52f6cd67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a8dd2c5223c7244a5414d0dda2696d",
+    "url": "/Ticker/index.html"
+  },
+  {
+    "revision": "32b73291b88d8e11771a",
+    "url": "/Ticker/static/css/main.416a75d9.chunk.css"
+  },
+  {
+    "revision": "272ebd98d955d1f89ee3",
+    "url": "/Ticker/static/js/2.58814ecf.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Ticker/static/js/2.58814ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b73291b88d8e11771a",
+    "url": "/Ticker/static/js/main.3e249fb8.chunk.js"
+  },
+  {
+    "revision": "9db34055de1f34da22a2",
+    "url": "/Ticker/static/js/runtime-main.97116166.js"
+  },
+  {
+    "revision": "c8730e0ac2d5e53955b48eca55dcecd2",
+    "url": "/Ticker/static/media/1.c8730e0a.jpg"
+  },
+  {
+    "revision": "74b3cd34496b211675bbaa5295683e19",
+    "url": "/Ticker/static/media/bg.74b3cd34.png"
+  },
+  {
+    "revision": "1d57288f5f439e5eb29f9b012d44d8c0",
+    "url": "/Ticker/static/media/post_bg.1d57288f.png"
+  },
+  {
+    "revision": "59df1d905054d5242fb2b81d0b040614",
+    "url": "/Ticker/static/media/title.59df1d90.png"
+  }
+]);
